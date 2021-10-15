f3464a5b905a0ae1489c9b2bd4597e91aa410ed6
Kamil <kamilpawlak@op.pl>
2021-10-15 15:30:08 +0200

    I add Constants with named colors I add whole Layout in SnapKit

---
+++ Calculator/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Calculator
+//
+//  Created by Kamil Pawlak on 15/10/2021.
+//
+import UIKit
+
+struct K {
+    
+    struct ButtonColour {
+        static let numbers = UIColor.init(named: K.Colour.lightGreen)
+        static let topRowOperations = UIColor.init(named: K.Colour.darkGreen)
+        static let rightColumnOperations = UIColor.init(named: K.Colour.lime)
+        static let background = UIColor.init(named: K.Colour.black)
+    }
+    
+    struct Colour {
+        static let lightGreen = "Light Green"
+        static let darkGreen = "Dark Green"
+        static let lime = "Lime"
+        static let black = "Black"
+        
+    }
+    
+}
+
+
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -6,14 +6,374 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Text Label Section
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = K.ButtonColour.background
+        return view
+    }()
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.text = "0"
+        label.font = .systemFont(ofSize: 50)
+        label.textAlignment = .right
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    //MARK: - First Row
+    
+    private let firstRowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 1
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let ACButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("AC", for: .normal)
+        button.backgroundColor = K.ButtonColour.topRowOperations
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let plusMinusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("+/-", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.topRowOperations
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let percentageButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("%", for: .normal)
+        button.backgroundColor = K.ButtonColour.topRowOperations
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let divisionButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("÷", for: .normal)
+        button.backgroundColor = K.ButtonColour.rightColumnOperations
+        button.titleLabel!.font = .systemFont(ofSize: 35)
+        button.setTitleColor(K.ButtonColour.background, for: .normal)
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Second Row
+    
+    private let secondRowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 1
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let numberSevenButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("7", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let numberEightButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("8", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let numberNineButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("9", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let multiplyButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("×", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 35)
+        button.backgroundColor = K.ButtonColour.rightColumnOperations
+        button.setTitleColor(K.ButtonColour.background, for: .normal)
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Third Row
+    
+    private let thirdRowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 1
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let numberFourButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("4", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let numberFiveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("5", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let numberSixButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("6", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let substractionButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("-", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 35)
+        button.backgroundColor = K.ButtonColour.rightColumnOperations
+        button.setTitleColor(K.ButtonColour.background, for: .normal)
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Fourth Row
+    private let fourthRowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 1
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let numberOneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("1", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let numberTwoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("2", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let numberThreeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("3", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let additionButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("+", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 35)
+        button.backgroundColor = K.ButtonColour.rightColumnOperations
+        button.setTitleColor(K.ButtonColour.background, for: .normal)
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Fifth Row
+    
+    private let fifthRowStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 1
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let numberZeroButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("0", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let dotEqualStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 1
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private let dotButton: UIButton = {
+        let button = UIButton()
+        button.setTitle(".", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 30)
+        button.backgroundColor = K.ButtonColour.numbers
+        button.addTarget(self, action: #selector(numberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    private let equalButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("=", for: .normal)
+        button.titleLabel!.font = .systemFont(ofSize: 35)
+        button.backgroundColor = K.ButtonColour.rightColumnOperations
+        button.setTitleColor(K.ButtonColour.background, for: .normal)
+        button.addTarget(self, action: #selector(nonNumberButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    //MARK: - Main Stack View
+    
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 1
+        return stackView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addAllComponentsToMainView()
     }
+    
+    //MARK: - Add all components to main view
+    
+    private func addAllComponentsToMainView() {
+        addBackgroundView()
+        addTextLabel()
+        addFirstStackView()
+        addSecondStackView()
+        addThirdStackView()
+        addFouthStackView()
+        addDotEqualStackView()
+        addFifthStackView()
+        addMainStackView()
+    }
+    
+    private func addBackgroundView() {
+        view.addSubview(backgroundView)
+        backgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(0)
+            $0.left.equalToSuperview().offset(0)
+            $0.right.equalToSuperview().offset(0)
+            $0.height.equalTo(150)
+        }
+    }
+    
+    private func addTextLabel() {
+        backgroundView.addSubview(textLabel)
+        textLabel.snp.makeConstraints {
+            $0.left.equalTo(backgroundView.snp.left).offset(10)
+            $0.right.equalTo(backgroundView.snp.right).offset(-10)
+            $0.bottom.equalTo(backgroundView.snp.bottom).offset(10)
+            $0.height.equalTo(100)
+        }
+    }
+    
+    private func addFirstStackView() {
+        let items = [ACButton, plusMinusButton, percentageButton, divisionButton]
+        for item in items{
+            firstRowStackView.addArrangedSubview(item)
+        }
+    }
+    
+    private func addSecondStackView() {
+        let items = [numberSevenButton, numberEightButton, numberNineButton, multiplyButton]
+        for item in items{
+            secondRowStackView.addArrangedSubview(item)
+        }
+    }
+    
+    private func addThirdStackView() {
+        let items = [numberFourButton, numberFiveButton, numberSixButton, substractionButton]
+        for item in items{
+            thirdRowStackView.addArrangedSubview(item)
+        }
+    }
+    
+    private func addFouthStackView() {
+        let items = [numberOneButton, numberTwoButton, numberThreeButton, additionButton]
+        for item in items{
+            fourthRowStackView.addArrangedSubview(item)
+        }
+    }
+    
+    private func addDotEqualStackView() {
+        let items = [dotButton, equalButton]
+        for item in items {
+            dotEqualStackView.addArrangedSubview(item)
+        }
+    }
+    
+    private func addFifthStackView() {
+        let items = [numberZeroButton, dotEqualStackView]
+        for item in items {
+            fifthRowStackView.addArrangedSubview(item)
+        }
+    }
+    
+    private func addMainStackView() {
+        view.addSubview(mainStackView)
+        let items = [firstRowStackView, secondRowStackView, thirdRowStackView, fourthRowStackView, fifthRowStackView]
+        for item in items {
+            mainStackView.addArrangedSubview(item)
+        }
+        mainStackView.snp.makeConstraints{
+            $0.top.equalTo(backgroundView.snp.bottom).offset(0)
+            $0.left.equalToSuperview().offset(0)
+            $0.right.equalToSuperview().offset(0)
+            $0.bottom.equalToSuperview().offset(0)
+        }
+    }
+    
+    //MARK: - Functions
+    
+    @objc private func nonNumberButtonPressed(sender: UIButton!) {
+    
+    }
+    
+    @objc private func numberButtonPressed(sender: UIButton!) {
 
-
+    }
+    
 }
-
