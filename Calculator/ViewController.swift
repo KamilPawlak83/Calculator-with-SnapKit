60556ecd045b1706ef8d521d18bab96d11c0dc34
Kamil <kamilpawlak@op.pl>
2021-10-15 11:49:07 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kamil Pawlak on 15/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
